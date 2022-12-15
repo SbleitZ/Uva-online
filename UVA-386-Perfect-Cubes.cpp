155971ae7ce58307f9ea79fa62f1ef3791c5fa2e
Sbleit <72667973+SbleitZ@users.noreply.github.com>
2022-12-14 23:14:53 -0300

    add perfect cubes

---
+++ UVA-386-Perfect-Cubes.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int n = 200;
+    int max_iterable = 200;
+    // int respuesta[2];
+    for(int i = 1;i<=n;i++){
+        for(int a = 2;a<max_iterable;a++){
+        for(int b = 2;b<max_iterable;b++){
+            for(int c = 2;c<max_iterable;c++){
+                int aux = a*a*a + b*b*b + c*c*c;
+                if(i*i*i == aux && (a<b && b<c && a<c)){
+                    printf("Cube = %i, Triple = (%i,%i,%i)\n",i,a,b,c);
+                }
+            }
+        }
+        // cout<<"entro";
+    }
+    }
+
+    return 0;
+}
